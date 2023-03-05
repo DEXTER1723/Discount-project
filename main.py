550d094df1b0604a811a7702a2b67809ea34fe57
DEXTER1723 <105650086+DEXTER1723@users.noreply.github.com>
2023-03-04 23:23:04 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+print("\n---Alimony firm---\n \n Welcome single person, We know that your preferences are in the price! \n for inquiries contact us, line 801\n-------------------------------------" )  
+productPrice = float(input("enter the value of the item: \n> "))  
+discoutPercent = 7
+discount = discoutPercent/100*productPrice
+print("\nThe discount is:", discount)
+print("\nTotal that you need to pay",productPrice+discount,"\n")
